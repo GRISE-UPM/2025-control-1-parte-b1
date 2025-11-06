bbc1d772a400124cbe4176f87e8579691c6790ea
kohee-dev <abartolome215@gmail.com>
2025-11-06 18:36:52 +0100

    Project converted to maven

--- src/test/es/grise/upm/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/es/grise/upm/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,5 +1,12 @@
 package es.grise.upm.profundizacion.subscriptionService;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
 public class SubscriptionServiceTest {
-	
+
+    @Test
+    public void testSubscriptionService() {
+
+    }
 }
