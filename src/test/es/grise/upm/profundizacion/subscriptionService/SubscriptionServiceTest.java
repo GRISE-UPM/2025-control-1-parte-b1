4a739752b40e8956ef7ff36e18fdddfef98adb5a
kohee-dev <abartolome215@gmail.com>
2025-11-06 19:00:51 +0100

    Added unit tests to SubscriptionService

--- src/test/es/grise/upm/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/es/grise/upm/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,12 +1,59 @@
 package es.grise.upm.profundizacion.subscriptionService;
 
+import es.grise.upm.profundizacion.exceptions.ExistingUserException;
+import es.grise.upm.profundizacion.exceptions.LocalUserDoesNotHaveNullEmailException;
+import es.grise.upm.profundizacion.exceptions.NullUserException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SubscriptionServiceTest {
 
-    @Test
-    public void testSubscriptionService() {
+    private User user = new User() {
+        @Override
+        public String getEmail() {
+            return "hola@hola.test";
+        }
 
+        @Override
+        public Delivery getDelivery() {
+            return Delivery.LOCAL;
+        }
+    };
+
+    private Delivery delivery;
+
+    @Test
+    public void addInvalidSubscriberTest() throws NullUserException, LocalUserDoesNotHaveNullEmailException, ExistingUserException {
+        SubscriptionService subscriptionService = new SubscriptionService(delivery);
+        assertThrows(NullUserException.class,() -> subscriptionService.addSubscriber(null));
+    }
+
+    @Test
+    public void addExistingSubscriberTest() throws NullUserException, ExistingUserException {
+        //Create hashset with mock user
+        Collection<User> subscribers = new HashSet<User>();
+        subscribers.add(user);
+        //Create subscriptionService with user
+        SubscriptionService subscriptionService = new SubscriptionService(delivery);
+        subscriptionService.setSubscribers(subscribers);
+
+        assertThrows(ExistingUserException.class,() -> subscriptionService.addSubscriber(user));
+    }
+
+    @Test
+    public void localUserNeedsNullEmailTest() throws NullUserException, ExistingUserException {
+        SubscriptionService subscriptionService = new SubscriptionService(delivery);
+        assertThrows(LocalUserDoesNotHaveNullEmailException.class,() -> subscriptionService.addSubscriber(user));
+    }
+
+    @Test
+    public void addUserTest() throws NullUserException, ExistingUserException, LocalUserDoesNotHaveNullEmailException {
+        SubscriptionService subscriptionService = new SubscriptionService(delivery);
+        subscriptionService.addSubscriber(user);
+        assertEquals(1, subscriptionService.getSubscribers().size());
     }
 }
