aae6f8c56856d158a30f7186b161f805fb8684d3
210373 <jorge.morenor@alumnos.upm.es>
2025-11-06 18:38:12 +0100

    Proyecto con maven el anterior lo hice mal

---
+++ control/src/main/es/grise/upm/profundizacion/subscriptionService/User.java
@@ -0,0 +1,10 @@
+package es.grise.upm.profundizacion.subscriptionService;
+
+public interface User {
+	
+	Delivery delivery = null;
+
+	public String getEmail();
+	public Delivery getDelivery();
+	
+}
